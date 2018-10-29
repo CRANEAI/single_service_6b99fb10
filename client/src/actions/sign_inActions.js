2a095804fd3d23474b8a5c6c7227bbfb26718cfc
Elly <elly@mg.crane.ai>
2018-10-29 17:33:21 -0400

    updated client/src/actions/sign_inActions refactoring

--- client/src/actions/sign_inActions.js
+++ client/src/actions/sign_inActions.js
@@ -3,3 +3,21 @@
             */
 import axios from "axios"
 import { client, url, setToken } from "../utils/axios.config.js"
+
+export const POSTLOGIN = "POSTLOGIN"
+
+export function postLogin() {
+  return axios
+    .post(url + "/user/postLogin")
+    .then(res => {
+      return {
+        type: POSTLOGIN,
+        payload: res.data
+      }
+    })
+    .catch(err => {
+      if (err.response.status === 401) {
+        //handle unauthorized access
+      }
+    })
+}
