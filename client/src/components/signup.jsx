f03dc823d08db65345a2596d61b87db1f695ecc3
Elly <elly@mg.crane.ai>
2018-10-29 18:10:24 -0400

    updated client/src/components/signup refactoring

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -7,13 +7,16 @@ created by: FusionCode
 // Component Requirements
 //----------------------------------------------------------------------------------------------------------
 
-sign in view has Label located at the top center of the view, text needs identified
-sign in view has Label located at the top center of the view, text needs identified
-the sign in view has a Input Box located at the mid-top in the center of the view, correct text needs defined
-sign in view has Label located at the mid-top left of the view, text needs identified
-sign in view has Label located at the main area left of the view, text needs identified
-sign in view has Label located at the bottom center of the view, text needs identified
+following view has an Image located at the top center of the view. this is a placeholder and should be updated with higher resolution Image
+following view has Label located at the mid-top left of the view, text needs identified
+following view has Label located at the main area left of the view, text needs identified
+following view has Label located at the main area left of the view, text needs identified
+following view has a Button located at the mid-bottom center of the view with text login
+following view has a Label located at the bottom center of the view with text sign up
+following view has Button located at the main area center of the view, text for button needs identified
 view has actions which trigger on events, view should should error messages and display activity indicator when processing
+the signup view has a profile icon located at the very-top center of the view which on click will trigger the profile() action
+the sign in view has a profile icon located at the very-top center of the view which on click will trigger the profile() action
 
 */
         
@@ -74,6 +77,26 @@ import { Row, Col , Button } from 'reactstrap';
                     <Col md={12}>
 
                         				<Button className="btn "><!-- --></Button> 
+
+			</Col>
+
+			<Col md={12}>
+				<p> sign up </p>
+			</Col>
+
+			<Col md={12}>
+				{/* <!-- TODO: Create unit test for login action --> */}
+				<Button 
+                        className="btn btn-lg btn-block " 
+                        onClick={() => this.login()}>login</Button> 
+
+			</Col>
+
+			<Col md={12}>
+				{/* <!-- TODO: Create unit test for sign_up action --> */}
+				<Button 
+                        className="btn btn-lg btn-block " 
+                        onClick={() => this.sign_up()}>sign up</Button> 
 
 			</Col>
 
