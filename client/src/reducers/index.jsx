43a2a8002d50453e03ed9fb41a67768e7d0e5298
Elly <elly@mg.crane.ai>
2018-10-29 18:17:44 -0400

    updated client/src/reducers/index refactoring

--- client/src/reducers/index.jsx
+++ client/src/reducers/index.jsx
@@ -3,14 +3,14 @@ import { reducer as Form } from "redux-form"
 
 import sign_in from "./reducer_sign_in"
 import signup from "./reducer_signup"
-import following from "./reducer_following"
+import login from "./reducer_login"
 import sign_up from "./reducer_sign_up"
 import sign_up from "./reducer_sign_up"
 
 const rootReducer = combineReducers({
   sign_in: sign_in,
   signup: signup,
-  following: following,
+  login: login,
   sign_up: sign_up,
   sign_up: sign_up
 })
