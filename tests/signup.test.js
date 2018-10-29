df669f21b480c6c3f9e70aba40402ccd20fa8fa6
Elly <elly@mg.crane.ai>
2018-10-29 18:13:12 -0400

    updated tests/signup refactoring

--- tests/signup.test.js
+++ tests/signup.test.js
@@ -7,10 +7,10 @@
  * single service
 */
 
-/* view has actions which trigger on events, view should should error messages and display activity indicator when processing */
-it("view has actions which trigger on events, view should should error messages and display activity indicator when processing", () => {
+/* the sign in view has a profile icon located at the very-top center of the view which on click will trigger the profile() action */
+it("the sign in view has a profile icon located at the very-top center of the view which on click will trigger the profile() action", () => {
   /* ToDo developer needs to complete unit test 
-               view has actions which trigger on events, view should should error messages and display activity indicator when processing
+               the sign in view has a profile icon located at the very-top center of the view which on click will trigger the profile() action
                uncomment below and update the code to test
                 //instance.action(function(error) {
                 //  if (error) throw error;
