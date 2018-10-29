5de2446a77cd32b905b58969b6959b14b4d35c13
Elly <elly@mg.crane.ai>
2018-10-29 18:11:21 -0400

    added tests/signup refactoring

---
+++ tests/signup.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - signup UNIT TESTS
+ * Application Client
+ * single service
+*/
+
+/* the sign in view has a profile icon located at the very-top center of the view which on click will trigger the profile() action */
+it("the sign in view has a profile icon located at the very-top center of the view which on click will trigger the profile() action", () => {
+  /* ToDo developer needs to complete unit test 
+               the sign in view has a profile icon located at the very-top center of the view which on click will trigger the profile() action
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
